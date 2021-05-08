563f838ebfd17cb179059f6f15f89d084f5916b6
huajianzhang <278554189@qq.com>
2021-05-08 09:15:33 +0800

    用bootstrap5写了下样式!

--- Three/Models/Department.cs
+++ Three/Models/Department.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,7 @@ namespace Three.Models
     public class Department
     {
         public int Id { get; set; }
+        [Display(Name ="部门名称")]
         public string Name { get; set; }
         public string Location { get; set; }
         public int EmployeeCount { get; set; }

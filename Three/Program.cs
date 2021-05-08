6caf0c6aec4848b16a03d67d6676b3c1ec824d75
huajianzhang <278554189@qq.com>
2021-05-08 10:54:23 +0800

    收工

--- Three/Program.cs
+++ Three/Program.cs
@@ -18,6 +18,12 @@ namespace Three
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configBuilder) =>
+            {
+                // 自己写的配置文件
+                // configBuilder.Sources.Clear();
+                configBuilder.AddJsonFile("nick.json");
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Three/Startup.cs
+++ Three/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,6 +14,14 @@ namespace Three
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            //var three = _configuration["Three:BoldDepartmentEmployeeCountThreshold"];
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,6 +30,7 @@ namespace Three
             services.AddSingleton<IClock,ChinaClock>();
             services.AddSingleton<IDepartmentService, DepartmentService>();
             services.AddSingleton<IEmployeeService, EmployeeService>();
+            services.Configure<ThreeOptions>(_configuration.GetSection("Three"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
